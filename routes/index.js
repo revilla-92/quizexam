08a5954f0ba544ad6179a13b7a5ba4d41cd9ea85
revilla-92 <daniel.revilla.twose@gmail.com>
2016-01-26 20:31:09 +0100

    Added forms for login and register

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Ponemos los controladores para las peticiones que se pueden realizar.
+var userController = require('../controllers/user_controller');
+var sessionController = require('../controllers/session_controller');
+
 
 /************************** GET Home Page. ***************************/
 router.get('/', function(req, res) {
@@ -16,16 +20,22 @@ router.get('/autor', function(req,res,next) {
 /*********************************************************************/
 
 
-/************************ Rutas de las sesiones **********************/
+/************************** Rutas del login **************************/
 
-// Obtener el formulario a rellenar para hacer login.
-// router.get('/login',  sessionController.new); 
-             
-// Enviar formulario para crear la sesión.
-// router.post('/login', sessionController.create); 
+// Mostrar el formulario para registrarse (signup).
+router.get('/signup', function(req, res, next) {
+	res.render('users/new');
+});
 
-// Destruir la sesión actual.
-// router.get('/logout', sessionController.destroy); 
+// Mostrar el formulario para logear (login).
+router.get('/login_show', function(req, res, next) {
+	res.render('session/new');
+});
+
+router.get('/login', function(req, res, next){
+	
+})
+
 /*********************************************************************/
 
 
